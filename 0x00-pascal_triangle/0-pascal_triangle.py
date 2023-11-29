f8112ba05ea1bf1c7558570479d46d9c4981596c
humphrey abwao <humphreyabwao@humphreys-MacBook-Pro.local>
2023-11-29 20:09:43 +0300

    updated file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,24 @@
-def generate_pascal(n):
-    if n <= 0:
-        return []
-    result = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(result[i-1][j-1] + result[i-1][j])
-        row.append(1)
-        result.append(row)
-    return result
+#!/usr/bin/python3
+"""A script to determine pascal's triangle for any number"""
 
-print(generate_pascal(5))
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    triangle = []
+
+    # return (trianlgle if n <= 0)
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        temp_list = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
+    return triangle
